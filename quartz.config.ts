6984049dc2fe64ff7416dfe9bdc442d1ef795eb3
Ryan Wüest <ryan.wueest@protonmail.com>
2025-01-31 13:33:37 +0100

    Quartz sync: Jan 31, 2025, 1:33 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,36 +8,35 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "🧠 My Second Brain",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
-    analytics: {
-      provider: "plausible",
-    },
+    analytics: null,
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "wueestry.github.io/my-second-brain",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "JetBrains Mono",
+        body: "JetBrains Mono",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
-          light: "#faf8f8",
-          lightgray: "#e5e5e5",
-          gray: "#b8b8b8",
-          darkgray: "#4e4e4e",
-          dark: "#2b2b2b",
-          secondary: "#284b63",
-          tertiary: "#84a59d",
-          highlight: "rgba(143, 159, 169, 0.15)",
-          textHighlight: "#fff23688",
+          light: "#1e1e2e",
+          lightgray: "#313244",
+          gray: "#6c7086",
+          darkgray: "#a6adc8",
+          // dark: "#181825",
+          dark: "#cdd6f4",
+          secondary: "#89b4fa",
+          tertiary: "#a6e3a1",
+          highlight: "rgba(186, 194, 222, 0.15)",
+          textHighlight: "rgba(250, 179, 135, 0.2)",
         },
         darkMode: {
           light: "#161618",
@@ -50,6 +49,7 @@ const config: QuartzConfig = {
           highlight: "rgba(143, 159, 169, 0.15)",
           textHighlight: "#b3aa0288",
         },
+
       },
     },
   },
