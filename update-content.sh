181fc1e5e19463f261d336373257939d67fc91e3
Ryan Wüest <ryan.wueest@protonmail.com>
2025-05-12 09:22:59 +0200

    Quartz sync: May 12, 2025, 9:22 AM

--- update-content.sh
+++ update-content.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 cd content || git pull
+cd .scripts || python3 tag_sorter.py -p ".." || python3 reference_sorter.py -p ".."
+cd ..
 cd ..
 
 if command -v nix &> /dev/null; then
